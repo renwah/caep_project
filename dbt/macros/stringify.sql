0ebfbcf404b4a8492a8b67c59bba0b807f5e8b0f
Renee Wah <rwah@illinois.edu>
2026-07-30 15:02:55 -0500

    add stringified variables, update docs

--- dbt/macros/stringify.sql
+++ dbt/macros/stringify.sql
@@ -1,8 +1,8 @@
 {#
- stringify_terms(table, column) - returns a string combining the values of a column in a table, ordered by term, with no separator
+ stringify(table, column) - returns a string combining the values of a column in a table, ordered by term, with no separator
 #}
 
-{% macro stringify_terms(table, column) %}
+{% macro stringify(table, column) %}
                     string_agg(
                 {{ table }}.{{ column }}::varchar, '' order by {{ table }}.term
             ) as {{ column }}_by_terms
--- dbt/staging/students_course_info.sql
+++ dbt/staging/students_course_info.sql
@@ -17,25 +17,103 @@ Any both ESL enrollment per semester (1 for credit, 0 for not enrolled IN BOTH) 
         select
             e.student_uuid,
             e.gi03 as term,
-            bool_or(c.is_esl_course::bool) as any_esl_enrollment,
-            bool_or(c.is_noncredit_esl_course::bool) as any_noncredit_esl_enrollment,
-            bool_or(c.is_credit_esl_course::bool) as any_credit_esl_enrollment,
-            bool_or(
-                c.is_noncredit_esl_course::bool and c.is_credit_esl_course::bool
-            ) as any_both_esl_enrollment,
+            cast(bool_or(c.is_esl_course::bool) as int) as any_esl_enrollment,
+            cast(bool_or(c.is_noncredit_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')) as int) as any_noncredit_esl_enrollment,
+            cast(bool_or(c.is_credit_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')) as int) as any_credit_esl_enrollment,
+            cast(bool_or(
+                c.is_noncredit_esl_course::bool and c.is_credit_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')
+            ) as int) as any_both_esl_enrollment,
             -- only record for noncredit esl courses that are not integrated and are
             -- not incorrectly labeled Y level courses
+            max(
+                case
+                --record for noncredit esl courses that are not integrated. if not, then record null
+                    when c.is_noncredit_esl_course::bool and c.cb03 != '493087' and e.sx04 not in ('W', 'FW', 'DR')
+                    then case when c.esl_course_level_adjusted_for_college in ('Y') then '1'
+                    else c.esl_course_level_adjusted_for_college end
+                    else '0'
+                end
+            ) as lowest_cb21_level_adjusted_rws,
+            max(
+                case
+                    when c.is_noncredit_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')
+                    then case when c.esl_course_level_adjusted_for_college in ('Y') then '1'
+                    else c.esl_course_level_adjusted_for_college end
+                    else '0'
+                end
+            ) as lowest_cb21_level_adjusted_all,
+            max(
+                case
+                    when c.is_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')
+                    then case when c.esl_course_level_adjusted_for_college in ('Y') then '1'
+                    else c.esl_course_level_adjusted_for_college end
+                    else '0'
+                end
+            ) as lowest_cb21_level_adjusted_all_credit_types,
+            max(
+                case
+                    when c.is_noncredit_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')
+                    then 
+                        case 
+                        when c.esl_course_level_adjusted_for_college in ('Y') then '1'
+                        when c.esl_course_level_adjusted_for_college in ('G', 'H') then 'F'
+                        else c.esl_course_level_adjusted_for_college
+                        end
+                    else '0'
+                end
+            ) as lowest_cb21_level_adjusted_a_f_only_collapsed,
+            max(
+                case
+                    when c.is_noncredit_esl_course::bool and e.sx04 not in ('W', 'FW', 'DR')
+                    then 
+                        case 
+                            when c.esl_course_level_adjusted_for_college in ('Y') then '1'
+                            when c.esl_course_level_adjusted_for_college in ('G', 'H') then '0'
+                            else c.esl_course_level_adjusted_for_college
+                        end
+                    else '0'
+                end
+            ) as lowest_cb21_level_adjusted_a_f_only_others_0,
             min(
                 case
                 --record for noncredit esl courses that are not integrated. if not, then record null
-                    when c.is_noncredit_esl_course::bool and c.cb03 != '493087'
-                    then c.esl_course_level_adjusted_for_college
+                    when c.is_noncredit_esl_course::bool and c.cb03 != '493087' and e.sx04 not in ('W', 'NP', 'INP', 'FW', 'DR', 'F')  -- didn't fail/drop
+                    then case when c.esl_course_level_adjusted_for_college in ('Y') then null
+                    else c.esl_course_level_adjusted_for_college end
+                    else '0'
                 end
-                order by csm.levels_below_transfer
-            ) as first_cb21_level_adjusted_for_college,
-            case
-                when min(c.cb03) = '493087' then true else false
-            end as only_integrated_esl_course
+            ) as highest_cb21_level_adjusted_achieved_rws,
+            min(
+                case
+                --record for noncredit esl courses that are not integrated. if not, then record null
+                    when c.is_noncredit_esl_course::bool and e.sx04 not in ('W', 'NP', 'INP', 'FW', 'DR', 'F')  -- didn't fail/drop
+                    then case when c.esl_course_level_adjusted_for_college in ('Y') then null
+                    else c.esl_course_level_adjusted_for_college end
+                    else '0'
+                end
+            ) as highest_cb21_level_adjusted_achieved_all,
+            min(
+                case
+                --record for noncredit esl courses that are not integrated. if not, then record null
+                    when c.is_esl_course::bool and e.sx04 not in ('W', 'NP', 'INP', 'FW', 'DR', 'F')  -- didn't fail/drop
+                    then case when c.esl_course_level_adjusted_for_college in ('Y') then null
+                    else c.esl_course_level_adjusted_for_college end
+                    else '0'
+                end
+            ) as highest_cb21_level_adjusted_achieved_crnc,
+            case 
+                when bool_or(c.cb03 = '493084' and e.sx04 not in ('W', 'FW', 'DR')) then 1 else 0
+            end as any_493084_course_enrollment,
+            case 
+                when bool_or(c.cb03 = '493085' and e.sx04 not in ('W', 'FW', 'DR')) then 1 else 0
+            end as any_493085_course_enrollment,
+            case 
+                when bool_or(c.cb03 = '493086' and e.sx04 not in ('W', 'FW', 'DR')) then 1 else 0
+            end as any_493086_course_enrollment,
+            case 
+                when bool_or(c.cb03 = '493087' and e.sx04 not in ('W', 'FW', 'DR')) then 1 else 0
+            end as any_493087_course_enrollment
+
 
         -- cb21 level for each term, adjusted for college (A-F, G/H, Y, 0)
         -- any esl enrollment per term
@@ -44,38 +122,36 @@ Any both ESL enrollment per semester (1 for credit, 0 for not enrolled IN BOTH) 
         -- any both esl enrollment per term 
         from {{ source("caep_data", "sr1318sx") }} e
         join {{ ref("courses_cb21_adjusted") }} c on e.cb00 = c.cb00 and e.gi03 = c.term
-        full join
+        join
             {{ source("caep_data", "course_efl_score_mapping") }} csm
             on c.cb21 = csm.cb21
-            and ((c.cb21 != 'Y' and c.cb04 = 'N') or (c.cb03 != '493087'))  -- don't code Y level noncredit courses or integrated ESL courses
         where
             e.gi03 > 174
             and e.gi03 < 300  -- only include terms in the study window, up to 2030
             and e.sx02 = '19080808'  -- didn't drop
-            and e.sx04 not in ('W', 'FW', 'DR')  -- didn't withdraw/drop
         group by e.student_uuid, e.gi03
     ),
     student_codes as (
         select
             s.uuid,
-            string_agg(
-                p.any_esl_enrollment::int::varchar, '' order by p.term
-            ) as any_esl_enrollment_by_term,
-            string_agg(
-                p.any_noncredit_esl_enrollment::int::varchar, '' order by p.term
-            ) as any_noncredit_esl_enrollment_by_term,
-            string_agg(
-                p.any_credit_esl_enrollment::int::varchar, '' order by p.term
-            ) as any_credit_esl_enrollment_by_term,
-            string_agg(
-                p.any_both_esl_enrollment::int::varchar, '' order by p.term
-            ) as any_both_esl_enrollment_by_term,
-            string_agg(
-                p.first_cb21_level_adjusted_for_college, '' order by p.term
-            ) as first_cb21_level_adjusted_for_college_by_term,
-            string_agg(
-                p.only_integrated_esl_course::int::varchar, '' order by p.term
-            ) as bool_only_integrated_esl_course_by_term
+            {{ stringify('p','any_esl_enrollment') }}, 
+            {{ stringify('p','any_noncredit_esl_enrollment') }}, 
+            {{ stringify('p','any_credit_esl_enrollment') }},
+            {{ stringify('p','any_both_esl_enrollment') }},
+            {{ stringify('p','lowest_cb21_level_adjusted_rws')}}, 
+            {{ stringify('p','lowest_cb21_level_adjusted_all') }},
+            {{ stringify('p','lowest_cb21_level_adjusted_a_f_only_collapsed') }},
+            {{ stringify('p','lowest_cb21_level_adjusted_a_f_only_others_0') }},
+            {{ stringify('p','lowest_cb21_level_adjusted_all_credit_types') }},
+            {{ stringify('p','highest_cb21_level_adjusted_achieved_rws') }},
+            {{ stringify('p','highest_cb21_level_adjusted_achieved_all') }},
+            {# {{ stringify('p','highest_cb21_level_adjusted_achieved_crnc') }}, #}
+            {{ stringify('p','any_493084_course_enrollment') }},
+            {{ stringify('p','any_493085_course_enrollment') }},
+            {{ stringify('p','any_493086_course_enrollment') }},
+            {{ stringify('p','any_493087_course_enrollment') }}
+
+
         from students s
         left join per_term_info p on s.uuid = p.student_uuid
         group by s.uuid
@@ -83,11 +159,21 @@ Any both ESL enrollment per semester (1 for credit, 0 for not enrolled IN BOTH) 
 -- add fields to existing fields
 select
     s.*,
-    sc.any_esl_enrollment_by_term,
-    sc.any_noncredit_esl_enrollment_by_term,
-    sc.any_credit_esl_enrollment_by_term,
-    sc.any_both_esl_enrollment_by_term,
-    sc.first_cb21_level_adjusted_for_college_by_term,
-    sc.bool_only_integrated_esl_course_by_term
+    sc.any_esl_enrollment_by_terms,
+    sc.any_noncredit_esl_enrollment_by_terms,
+    sc.any_credit_esl_enrollment_by_terms,
+    sc.any_both_esl_enrollment_by_terms,
+    sc.lowest_cb21_level_adjusted_rws_by_terms, 
+    sc.lowest_cb21_level_adjusted_all_by_terms,
+    sc.lowest_cb21_level_adjusted_all_credit_types_by_terms,
+    sc.lowest_cb21_level_adjusted_a_f_only_collapsed_by_terms,
+    sc.lowest_cb21_level_adjusted_a_f_only_others_0_by_terms,
+    sc.highest_cb21_level_adjusted_achieved_rws_by_terms,
+    sc.highest_cb21_level_adjusted_achieved_all_by_terms,
+    {# sc.highest_cb21_level_adjusted_achieved_crnc_by_terms, #}
+    sc.any_493084_course_enrollment_by_terms,
+    sc.any_493085_course_enrollment_by_terms,
+    sc.any_493086_course_enrollment_by_terms,
+    sc.any_493087_course_enrollment_by_terms
 from student_codes sc
 join students s on sc.uuid = s.uuid
