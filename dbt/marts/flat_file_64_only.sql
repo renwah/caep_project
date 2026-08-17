3f8718844e54a8571cbc664ee45967af6ad57965
Renee Wah <rwah@illinois.edu>
2026-08-17 08:40:59 -0700

    add term observation flags

--- dbt/marts/flat_file_64_only.sql
+++ dbt/marts/flat_file_64_only.sql
@@ -4,6 +4,10 @@ select *, (
     multiple_colleges and
                                                               (college_group_enrollment_by_terms similar to '%O+' OR
                                                                college_group_enrollment_by_terms SIMILAR TO '%P+')
-) as non_64_college_enrollment
+) as non_64_college_enrollment,
+--cohort is long enough for 6 years observation based on 264 max gi03
+--check: count backward on sem_abs file
+(first_esl_term between 175 and 251) as observation_6_sem,
+(first_esl_term between 175 and 235) as observation_12_sem
 from {{ ref('students_course_info') }}
 where first_college_group IN ('CONFIRMED CC - 6 LEVELS', 'CONFIRMED CC - A/B', 'CONFIRMED CC - E/F')
